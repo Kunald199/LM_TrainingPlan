78e37d2dd7e77aa58829945669b585f0ef4562dd
Kunal Ajay Dhavale <kunaldhavale2@gmail.com>
2021-11-10 11:16:47 -0500

    First Commit
    First Commit

---
+++ LM_TrainingPlan1/src/main/java/com/example/demo/LmTrainingPlan1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmTrainingPlan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmTrainingPlan1Application.class, args);
+	}
+
+}
